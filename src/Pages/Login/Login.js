c32f8b68745ee4a4d7fb67ba28c3344ec02e4d6f
Amarnath <motorolaonefusion@gmail.com>
2023-01-15 11:32:44 +0530

    Upation in login ui

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,16 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Login.css'
 import AuthContext from '../../Context/AuthContext';
 import NavbarBanner from '../../Components/NavbarBanner';
 import Banner from '../../Components/Banner';
+import SnackBar from '../../Components/Snackbar/SnackBar';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function Login() {
 
@@ -14,30 +22,82 @@ function Login() {
     const handleLogin=(e)=>{
         e.preventDefault() 
         Userlogin(email,password)  
+        if (message){
+          setOpen(true);
+        }
     }
+
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault() 
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
 
   return (
     <div className='LoginPage'>
       <Banner/>
-      <form onSubmit={handleLogin} >
-        {message? <h1>{message}</h1> :''}
-        <label htmlFor="">Email</label>
-        <input type="email" name="email" id='email' 
-        onChange={(e)=>{
-            setemail(e.target.value)
-        }}/>
-        <label htmlFor="">Password</label>
-        <input type="password" name="password" id="password"
-        onChange={(e)=>{
-            setpassword(e.target.value)
-        }}
-         />
 
-        <button class="btn"  type="submit">Sign in</button>
-      </form>
+      <div className='login-section'>
+          <div className="Signin__details">
+            <div className="Signin__part">
+              <div className="Signin_section">
+                <div className="Signin__title">
+                  <h5>Already a member?</h5>
+                  <span>Sign In</span>
+                </div>
+
+                <div className="Signin_form">
+                <form onSubmit={handleLogin} >
+
+                    <input type="email" placeholder='Email' name="email" id='email' 
+                    onChange={(e)=>{
+                        setemail(e.target.value)
+                    }}/>
+
+                    <input type="password" placeholder='Password' name="password" id="password"
+                    onChange={(e)=>{
+                        setpassword(e.target.value)
+                    }}
+                    />
+
+                    <div className="signin_button">
+                      <button class="btn"  type="submit">Sign in</button>
+                    </div>
+                  </form> 
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          Username or Password is not valid
+        </Alert>
+      </Snackbar>
+    </Stack>
+      
     </div>
   )
 }
 
 export default Login
+
+
+
+
+
+
+
